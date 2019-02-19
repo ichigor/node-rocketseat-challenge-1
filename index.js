5207e7ea21946214fab49c59870ff7f12ae425f8
ichigor <igor.moiano@hotmail.com>
2019-02-18 23:07:34 -0300

    Routes working

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const nunjunks = require('nunjucks')
+
+const app = express()
+
+nunjunks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.set('view engine', 'njk')
+
+app.use(express.urlencoded({ extended: false }))
+
+const queryMiddleware = (req, res, next) => {
+  // req.query.age === undefined ? res.redirect('/') : next()
+  // console.log(req.body)
+  next()
+}
+
+app.get('/', (req, res) => {
+  return res.render('form')
+})
+
+app.get('/major', queryMiddleware, (req, res) => {
+  return res.render('major')
+})
+
+app.get('/minor', queryMiddleware, (req, res) => {
+  return res.render('minor')
+})
+
+app.post('/check', (req, res) => {
+  req.body.age >= 18 ? res.redirect('/major') : res.redirect('/minor')
+})
+
+app.listen(3000, () => {
+  console.log('server started')
+})
